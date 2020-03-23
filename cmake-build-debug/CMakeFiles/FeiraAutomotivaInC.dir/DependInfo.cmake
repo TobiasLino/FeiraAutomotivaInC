8aac47915e63e297b2a1f7d220c11d1ed51ac41c
TobiasLino <tobias.lino@fatec.sp.gov.br>
2020-03-22 23:16:16 -0300

    "Mudanças em .idea"

---
+++ cmake-build-debug/CMakeFiles/FeiraAutomotivaInC.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/tobias/Git/FeiraAutomotivaInC/src/include/list.c" "/home/tobias/Git/FeiraAutomotivaInC/cmake-build-debug/CMakeFiles/FeiraAutomotivaInC.dir/src/include/list.c.o"
+  "/home/tobias/Git/FeiraAutomotivaInC/src/model/address.c" "/home/tobias/Git/FeiraAutomotivaInC/cmake-build-debug/CMakeFiles/FeiraAutomotivaInC.dir/src/model/address.c.o"
+  "/home/tobias/Git/FeiraAutomotivaInC/src/model/client.c" "/home/tobias/Git/FeiraAutomotivaInC/cmake-build-debug/CMakeFiles/FeiraAutomotivaInC.dir/src/model/client.c.o"
+  "/home/tobias/Git/FeiraAutomotivaInC/src/model/phone.c" "/home/tobias/Git/FeiraAutomotivaInC/cmake-build-debug/CMakeFiles/FeiraAutomotivaInC.dir/src/model/phone.c.o"
+  "/home/tobias/Git/FeiraAutomotivaInC/src/model/schedule.c" "/home/tobias/Git/FeiraAutomotivaInC/cmake-build-debug/CMakeFiles/FeiraAutomotivaInC.dir/src/model/schedule.c.o"
+  "/home/tobias/Git/FeiraAutomotivaInC/src/teste.c" "/home/tobias/Git/FeiraAutomotivaInC/cmake-build-debug/CMakeFiles/FeiraAutomotivaInC.dir/src/teste.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../src/include"
+  "../src/model"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/FeiraAutomotivaInC.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FeiraAutomotivaInC.dir/src/include/list.c.o"
+  "CMakeFiles/FeiraAutomotivaInC.dir/src/model/address.c.o"
+  "CMakeFiles/FeiraAutomotivaInC.dir/src/model/client.c.o"
+  "CMakeFiles/FeiraAutomotivaInC.dir/src/model/phone.c.o"
+  "CMakeFiles/FeiraAutomotivaInC.dir/src/model/schedule.c.o"
+  "CMakeFiles/FeiraAutomotivaInC.dir/src/teste.c.o"
+  "FeiraAutomotivaInC"
+  "FeiraAutomotivaInC.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/FeiraAutomotivaInC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
